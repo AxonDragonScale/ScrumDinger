9c7ab93340cfd3702ba83806e379fa037e76c371
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-03-20 20:07:38 +0530

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Ronak Harkhani on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
